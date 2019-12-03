081ad67e24bfc6b28311897d06f02ea1a73bbfe7
Kevin Putera <kevintiosanna@gmail.com>
2019-12-03 16:46:28 +0800

    Add more data + update centering mechanism

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ import { shift } from "./utils";
 const AppContainer = styled.div`
   height: 100%;
   width: 100%;
+  overflow-y: auto;
 
   display: flex;
-  align-items: center;
-  justify-content: center;
+
+  & > * {
+    margin: auto;
+  }
 `;
 
 interface CustomItem {
@@ -22,10 +25,21 @@ interface CustomItem {
 
 const App: React.FC = () => {
   const [items, setItems] = useState<CustomItem[]>([
-    { key: 1, width: 240, height: 80, fillColor: "#293742" },
-    { key: 2, width: 240, height: 80, fillColor: "#5C7080" },
-    { key: 3, width: 240, height: 80, fillColor: "#BFCCD6" },
-    { key: 4, width: 240, height: 80, fillColor: "#D8E1E8" }
+    { key: 1, width: 240, height: 80, fillColor: "#182026" },
+    { key: 2, width: 240, height: 80, fillColor: "#202B33" },
+    { key: 3, width: 240, height: 80, fillColor: "#293742" },
+    { key: 4, width: 240, height: 80, fillColor: "#30404D" },
+    { key: 5, width: 240, height: 80, fillColor: "#394B59" },
+    { key: 6, width: 240, height: 80, fillColor: "#5C7080" },
+    { key: 7, width: 240, height: 80, fillColor: "#738694" },
+    { key: 8, width: 240, height: 80, fillColor: "#8A9BA8" },
+    { key: 9, width: 240, height: 80, fillColor: "#A7B6C2" },
+    { key: 10, width: 240, height: 80, fillColor: "#BFCCD6" },
+    { key: 11, width: 240, height: 80, fillColor: "#CED9E0" },
+    { key: 12, width: 240, height: 80, fillColor: "#D8E1E8" },
+    { key: 13, width: 240, height: 80, fillColor: "#E1E8ED" },
+    { key: 14, width: 240, height: 80, fillColor: "#EBF1F5" },
+    { key: 15, width: 240, height: 80, fillColor: "#F5F8FA" }
   ]);
 
   const getItemDimension = useCallback(
